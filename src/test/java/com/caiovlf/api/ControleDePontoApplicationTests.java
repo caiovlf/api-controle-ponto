1b003b74a0f523beff2b2c2a8a7f60a3f53be89b
Caio Vinicius <caiovlf@gmail.com>
2019-01-21 21:41:39 -0200

    First Commit

---
+++ src/test/java/com/caiovlf/api/ControleDePontoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.caiovlf.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ControleDePontoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
