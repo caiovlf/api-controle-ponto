a7661a28d6196dc0bb998612e4d00d8bd71e96cf
Caio Vinicius <caiovlf@gmail.com>
2019-01-30 22:13:29 -0200

    Criação de Entidades, Repositories, Tests, Configuração Banco H2 e PasswordUtils.

--- src/main/java/com/caiovlf/api/entities/Empresa.java
+++ src/main/java/com/caiovlf/api/entities/Empresa.java
@@ -2,12 +2,16 @@ package com.caiovlf.api.entities;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -23,6 +27,7 @@ public class Empresa implements Serializable {
 	private String cnpj;
 	private Date dataCriacao;
 	private Date dataAtualizacao;
+	private List<Funcionario> funcionarios;
 	
 	public Empresa() {
 	}
@@ -71,6 +76,15 @@ public class Empresa implements Serializable {
 
 	public void setDataAtualizacao(Date dataAtualizacao) {
 		this.dataAtualizacao = dataAtualizacao;
+	}
+
+	@OneToMany(mappedBy = "empresa", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	public List<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+
+	public void setFuncionarios(List<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
 	}
 	
 	@PreUpdate
---
+++ src/main/java/com/caiovlf/api/repositories/enums/PerfilEnum.java
@@ -0,0 +1,6 @@
+package com.caiovlf.api.repositories.enums;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+}
